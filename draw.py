36333fc121f6da864798fca3f9ad8022c2d8d0a4
Adrian Green <harvardsommer@gmx.net>
2015-06-15 15:40:53 +0100

    Added modules compute.py and draw.py

---
+++ draw.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Package: venn
+Module: draw
+
+This package provides functions for computing all sections of Venn diagrams.
+Functionality for drawing the diagrams in SVG format is also provided. Input
+can be sets or lists.
+
+The length of the input, i.e., the number of individual sets/lists is arbitrary.
+However, diagrmas can only be drawn up to length 5 - above that it gets too
+confusing anyway.
+
+Created on Mon Jun 15 15:34:04 2015
+
+@author: kp14
+"""
+from jinja2 import Environment, PackageLoader
+
+
