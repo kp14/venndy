3d78053829b5f85a2b6bfc71dd0e0411b07f2e95
kp14 <k14@ebi>
2018-04-18 11:22:32 +0100

    Tweak label positions in SVG. Version 1.3

--- venndy/__init__.py
+++ venndy/__init__.py
@@ -4,6 +4,8 @@ Created on Wed Jul 15 16:17:52 2015
 
 @author: kp14
 """
+from .draw import draw
+from .compute import compute_sections
 
 # The version as used in the setup.py
-__version__ = "1.2"
+__version__ = "1.3"
